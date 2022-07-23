9b6d6f6002cda528667d7672e157514d3294e632
foril <1571825323@qq.com>
2022-07-23 14:08:31 +0800

    chore: antd var config added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const path = require('path');
+const withPlugins = require('next-compose-plugins');
+const withAntdLess = require('next-plugin-antd-less');
 
+const pluginAntdLess = withAntdLess({
+  // @ts-ignore
+  modifyVars: {
+    '@primary-color': 'red',
+    '@menu-dark-bg': '#ccc'
+  }
+});
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
@@ -9,4 +18,5 @@ const nextConfig = {
   }
 }
 
-module.exports = nextConfig
+
+module.exports = withPlugins([[pluginAntdLess]], nextConfig);
